ffed9407846733a5bb44ebe07093069d8a530187
Anali Estrada Sandoval <analiestrada44@gmail.com>
2023-05-08 22:20:01 -0700

    tasklister lab completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener('DOMContentLoaded', () => {
+const getForm = document.querySelector("#create-task-form");
+getForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let newTask = document.getElementById("new-task-description").value
+    displayTask(newTask)
+})
+})
+
+function displayTask(newTask){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', deleteTask)
+  btn.textContent = 'x'
+  p.textContent = `${newTask}  `
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#tasks').appendChild(p)
+}
+
+function deleteTask(e){
+  e.target.parentNode.remove()
+}
+
+
+
+
